453ce482cca47bf55bbd02cc06074b61f9f78fec
Subrahmanya Vaidya <subrahmanyavaidya7@gmail.com>
2025-02-18 21:49:41 +0530

    fix/added Joi for security of auth

--- Backend/Routes/userRoutes.js
+++ Backend/Routes/userRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { login, signup, getUser } from '../Controllers/userController.js';
-import { loginValidation } from '../Controllers/authValidations.js';
+import { loginValidation , signupValidation } from '../Controllers/authValidations.js';
 
 const router = express.Router();
 
@@ -8,7 +8,7 @@ const router = express.Router();
 router.post('/login', loginValidation , login);
 
 // Signup route
-router.post('/signup', signup);
+router.post('/signup', signupValidation , signup);
 
 // Get user route
 // router.get('/getUser', getUser);
